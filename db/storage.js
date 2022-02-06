b5060c5ae3d2f280c65c710eb560b818fc688158
Cysmi <cnb10100@gmail.com>
2022-02-05 23:44:19 -0500

    update github repo with working code

--- db/storage.js
+++ db/storage.js
@@ -6,40 +6,39 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Storage {
-    read() {
-        return readFileAsync("db/db.json", "utf8")
-    }
-    write(note) {
-        return writeFileAsync("db/db.json", JSON.stringify(note))
-    }
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
-    addNote(note) {
-        const { title, text } = note
+  addNote(note) {
+    const { title, text } = note;
 
-        if (!title || !text) {
-            throw new Error("Title and text must have input!")
-        }
-
-        const newNote = { title, text, id: uuid() }
-
-        return this.getNotes()
-            .then(notes => [...notes, newNote])
-            .then(updatedNotes => this.write(updatedNotes))
-            .then(() => this.newNote)
+    if (!title || !text) {
+      throw new Error("Title and text must have input!");
     }
 
-    getNotes() {
-        return this.read()
-            .then(notes => {
-                return JSON.parse(notes) || [];
-            })
-    }
-    
-    removeNote(id) {
-        return this.getNotes()
-            .then(notes => notes.filter(note => note.id !== id))
-            .then(keptNotes => this.write(keptNotes))
-    }
+    const newNote = { title, text, id: uuid() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => this.newNote);
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      return JSON.parse(notes) || [];
+    });
+  }
+
+  removeNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((keptNotes) => this.write(keptNotes));
+  }
 }
 
 module.exports = new Storage();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,41 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const storage = require('../db/storage');
+const storage = require("../db/storage");
 
 // requesting the existing notes
 
-router.get('/notes', (req, res) => {
-    storage
-        .getNotes()
-        .then(notes => {
-            res.json(notes)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
+router.get("/notes", (req, res) => {
+  storage
+    .getNotes()
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-// posting note function route 
+// posting note function route
 
-router.post('/notes', (req, res) => {
-    console.log(req.body)
-    storage
-        .addNote(req.body)
-        .then(note => {
-            res.json(note)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  storage
+    .addNote(req.body)
+    .then((note) => {
+      res.json(note);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 // delete note function route
 
-router.delete('/notes/:id', (req, res) => {
-    storage
-        .removeNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err))
-})
+router.delete("/notes/:id", (req, res) => {
+  storage
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
